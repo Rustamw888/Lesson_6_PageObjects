cc142b3380728116f3c6942d19764ad9b0399396
QAmilord <i@qamilord.ru>
2022-04-10 23:39:18 +0300

    home work precondition step again

--- src/test/java/qa/guru/tests/RegistrationFormTests.java
+++ src/test/java/qa/guru/tests/RegistrationFormTests.java
@@ -1,4 +1,51 @@
 package qa.guru.tests;
 
+import org.junit.jupiter.api.BeforeAll;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class RegistrationFormTests {
+
+    @BeforeAll
+    public static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1980x1080";
+    }
+
+    @Test
+    public void fieldFormTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        
+        $("#firstName").setValue("Rustam");
+        $("#lastName").setValue("Tyapaev");
+        $("#userEmail").setValue("test@test.com");
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("1988");
+        $(".react-datepicker__month-select").selectOption("December");
+        $(".react-datepicker__day--019:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("pictures/1.png");
+        $("#currentAddress").setValue("Some address");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Rustam Tyapaev"), text("test@test.com"), text("Other"));
+        $(byText("Student Name")).parent().shouldHave(text("Rustam Tyapaev"));
+        $("#closeLargeModal").click();
+    }
 }
